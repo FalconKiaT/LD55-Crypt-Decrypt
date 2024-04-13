0ba29fff46a4d1890d5355cce56db72b17ace19d
FalconKiaT <117485567+FalconKiaT@users.noreply.github.com>
2024-04-13 12:45:44 -0700

    Added Placeholder Tilemap

--- Assets/Scripts/Entities/Enemies/Big Enemy.cs
+++ Assets/Scripts/Entities/Enemies/Big Enemy.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerStats : Entity
+public class BigEnemy : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
