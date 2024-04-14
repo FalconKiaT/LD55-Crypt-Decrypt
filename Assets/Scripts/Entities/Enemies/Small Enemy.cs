2744baf667c5a0cdd9d727d4021500d5ba399814
FalconKiaT <117485567+FalconKiaT@users.noreply.github.com>
2024-04-14 02:20:50 -0700

    Unit Management Progress

--- Assets/Scripts/Entities/Enemies/Small Enemy.cs
+++ Assets/Scripts/Entities/Enemies/Small Enemy.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SmallEnemy : MonoBehaviour
+public class SmallEnemy : Entity
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
