7d91c696a8aebd0b3accc24de65b1fa32c731663
Ryan Carpenter <dev.ryan1184@gmail.com>
2024-04-15 01:30:37 -0700

    Basic Combat

--- Assets/Scripts/Entities/Parent Classes/Entity.cs
+++ Assets/Scripts/Entities/Parent Classes/Entity.cs
@@ -10,10 +10,34 @@ public enum EntityTypes
     Catapult,
     ArmoredBoneman,
     Bomb,
-    SmallEnemy
+    Paladin,
+    Player,
+    Crate
 }
 
-public class Entity : FKMonoBehaviour
+public abstract class Entity : FKMonoBehaviour, ISelectable, ICommandable, IDamageable
 {
     public EntityTypes entityType;
+
+    public int health = 0;
+    public int damage = 0;
+
+    public virtual void TakeDamage(int damage)
+    {
+        if (damage >= health)
+        {
+            Death();
+        }
+        else
+            health -= damage;
+    }
+
+    public virtual void Death()
+    {
+        Destroy(gameObject);
+    }
+
+    public virtual void OnSelected() { }
+
+    public virtual void OnCommand(CommandType inputCommand, Vector2 target, Entity targetEntity) { }
 }
--- Assets/Scripts/Entities/Summonables/Catapult.cs
+++ Assets/Scripts/Entities/Summonables/Catapult.cs
@@ -2,20 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Catapult : Entity, ISelectable, ICommandable
+public class Catapult : Summonable
 {
-    private void Awake()
+    public override void OnCommand(CommandType inputCommand, Vector2 target, Entity targetEntity)
     {
-
-    }
-
-    public void OnSelected()
-    {
-        UnitManager.instance.AddToSelected(this);
-    }
-
-    public void OnCommand(CommandType inputCommand, Vector2 target, Entity targetEntity)
-    {
-
+        switch (inputCommand)
+        {
+            case CommandType.MoveTo:
+                MoveTo(target);
+                break;
+            case CommandType.ReleaseCarry:
+                // TODO: IMPLEMENT
+                break;
+            case CommandType.Carry:
+                // TODO: IMPLEMENT
+                break;
+            case CommandType.Stack:
+                // TODO: IMPLEMENT
+                break;
+            case CommandType.BoardCatapult:
+                // TODO: IMPLEMENT
+                break;
+        }
     }
 }
