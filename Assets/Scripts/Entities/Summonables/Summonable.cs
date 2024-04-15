d4e6eb6f6eb3dd77dd387f202950bc5d34f9fb03
FalconKiaT <117485567+FalconKiaT@users.noreply.github.com>
2024-04-15 14:55:30 -0700

    Attempted to fix pathfinder

--- Assets/Scripts/Entities/Summonables/Summonable.cs
+++ Assets/Scripts/Entities/Summonables/Summonable.cs
@@ -12,17 +12,8 @@ public abstract class Summonable : Entity
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Populate Dictionary of valid commands for the boneman
-    private void Awake()
-    {
-        // Set variables
-
-        // Populate 
-    }
-
     // Local Variables
     private Coroutine TESTINGROUTINE = null;
-
 
     public override void OnSelected()
     {
--- Assets/Scripts/UI/CameraFollow.cs
+++ Assets/Scripts/UI/CameraFollow.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    private Vector3 offset = new Vector3(0f, 1.75f, -10f);
+    [SerializeField] private Vector3 offset;
     private float smoothTime = 0.25f;
     private Vector3 velocity = Vector3.zero;
 
