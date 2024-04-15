4b113fdb0b9ded91757fd6e5dbd5dca694781ea7
Ryan Carpenter <dev.ryan1184@gmail.com>
2024-04-15 05:08:42 -0700

    Attack System Finished & Enemy Patrol

--- Assets/Scripts/Entities/Parent Classes/Entity.cs
+++ Assets/Scripts/Entities/Parent Classes/Entity.cs
@@ -6,7 +6,7 @@ using FKTools;
 public enum EntityTypes
 {
     NULL,
-    Boneman,
+    BasicBoneman,
     Catapult,
     ArmoredBoneman,
     Bomb,
@@ -22,6 +22,13 @@ public abstract class Entity : FKMonoBehaviour, ISelectable, ICommandable, IDama
     public int health = 0;
     public int damage = 0;
 
+    [SerializeField] private float deathDelay = 0;
+
+    public bool canFight;
+
+    protected Animator animator;
+    protected Rigidbody2D rb;
+
     public virtual void TakeDamage(int damage)
     {
         if (damage >= health)
@@ -34,6 +41,18 @@ public abstract class Entity : FKMonoBehaviour, ISelectable, ICommandable, IDama
 
     public virtual void Death()
     {
+        StartCoroutine(AttackCo());
+    }
+
+    public void AttackAnim(Vector2 attackDirection)
+    {
+        animator.SetFloat("directionX", Mathf.Sign(attackDirection.x));
+        animator.SetTrigger("attackTrigger");
+    }
+
+    private IEnumerator AttackCo()
+    {
+        yield return FKRoutines.WaitForSecondsPauseAware(deathDelay);
         Destroy(gameObject);
     }
 
