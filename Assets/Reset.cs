1959ce2f5ca74cc55f232458633f9acc82dc2a73
whatiswhatlol <bradydun29@gmail.com>
2024-04-15 18:41:04 -0700

    Added reset

---
+++ Assets/Reset.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reset : MonoBehaviour
+{
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R)) 
+        {
+        gameObject.GetComponent<PlayerStats>().Death();
+        }
+    }
+}
